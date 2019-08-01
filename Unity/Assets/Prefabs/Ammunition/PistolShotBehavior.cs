9c629316616bf18d19e2713f9b71d903d3de4f01
Kim Lillås <kim.lillas@gmail.com>
2019-08-01 19:34:00 +0300

    Add gunshot sound

--- Unity/Assets/Prefabs/Ammunition/PistolShotBehavior.cs
+++ Unity/Assets/Prefabs/Ammunition/PistolShotBehavior.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 namespace Ammunition
 {
+    [RequireComponent(typeof(AudioSource))]
     public class PistolShotBehavior : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
         {
-
+            GetComponent<AudioSource>().Play();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
         }
     }
 }
